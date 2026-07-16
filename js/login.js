9b333d05ecec6db8a7166741a1777ebcf00db572
Igreja Proposito Em Cristo <kaioeduardosilva1234@gmail.com>
2026-07-16 15:20:17 -0300

    Criacao painel gestao MEP

---
+++ js/login.js
@@ -0,0 +1,105 @@
+// =================================
+// LOGIN MEP ONLINE
+// =================================
+
+
+const btnLogin = document.getElementById("btnLogin");
+
+
+btnLogin.addEventListener("click", async ()=>{
+
+
+const email = document.querySelector(
+'input[type="email"]'
+).value;
+
+
+const senha = document.querySelector(
+'input[type="password"]'
+).value;
+
+
+
+if(!email || !senha){
+
+alert("Preencha email e senha");
+
+return;
+
+}
+
+
+
+
+const {data,error} = await supabaseClient.auth.signInWithPassword({
+
+email: email,
+
+password: senha
+
+});
+
+
+
+
+if(error){
+
+alert("Email ou senha incorretos");
+
+console.error(error);
+
+return;
+
+}
+
+
+
+
+const usuario = await supabaseClient
+
+.from("usuarios")
+
+.select("*")
+
+.eq(
+"auth_id",
+data.user.id
+)
+
+.single();
+
+
+
+
+if(usuario.error){
+
+alert("Usuário sem permissão cadastrada");
+
+return;
+
+}
+
+
+
+
+if(usuario.data.tipo_acesso === "gestao"){
+
+
+window.location.href="gestao.html";
+
+
+}
+
+
+
+if(usuario.data.tipo_acesso === "igreja"){
+
+
+window.location.href="portal.html";
+
+
+}
+
+
+
+});
