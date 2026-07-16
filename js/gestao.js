fd24fcefd0c32dc8c33c63c428d52530ba9af64e
Igreja Proposito Em Cristo <kaioeduardosilva1234@gmail.com>
2026-07-16 15:01:17 -0300

    Criacao painel gestao MEP

---
+++ js/gestao.js
@@ -0,0 +1,38 @@
+const botaoCurso = document.getElementById("novoCurso");
+
+
+if(botaoCurso){
+
+
+botaoCurso.addEventListener("click",()=>{
+
+
+alert("Abrir cadastro de curso");
+
+
+});
+
+
+}
+
+// ===============================
+// CURSOS
+// ===============================
+
+
+const botaoCurso = document.getElementById("novoCurso");
+
+
+if(botaoCurso){
+
+
+    botaoCurso.addEventListener("click",()=>{
+
+
+        alert("Abrir cadastro de curso");
+
+
+    });
+
+
+}
