cf3eb221f4bafab158339d264f467785ac09a802
Igreja Proposito Em Cristo <kaioeduardosilva1234@gmail.com>
2026-07-16 15:50:43 -0300

    Atualizacao

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://urkdnsewrdojczgmikub.supabase.co";
+const supabaseKey = "sb_publishable_2rd-7lLt2HcWDO2O7PwXYQ_4o7TzVmd";
+
+const supabaseClient = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
